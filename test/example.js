71e8ad0e82d4a094a1266fb50632e8fc8a469538
Eric Luo <eric_luo@Ymail.com>
2017-04-17 02:09:08 -0700

    Basic Setup + Travis CI

---
+++ test/example.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Example', function() {
+  describe('#SampleTest()', function() {
+    it('This is just a sample test that should be automatically passed', function() {
+      assert.equal(1, 1);
+    });
+  });
+});
