c7f4792b991c85e23d6702ddd761cb91b1b1868b
Moyang Wang <mowang@ucsd.edu>
2017-04-18 16:42:27 -0700

    winter is coming temporary change

--- test/foo_test.js
+++ test/foo_test.js
@@ -7,53 +7,51 @@ var foo = require('../routes/foo')
 
 describe('Foo Tests', function() {
     describe('URL Requests For bar = 5', function() {
-        var url = "http://localhost:3000/foo?bar=5";
-        
+        var url = "https://team0x13.herokuapp.com/foo?bar=5";
+
         it('returns status 200', function() {
             request(url, function(error, response, body){
                 assert.equal(response.statusCode,200);
             });
         });
-        
+
         it('return false for non string input', function() {
             request(url, function(error, response, body){
                 assert.equal(response.body, 'false');
             });
-        });  
-    });   
-    
+        });
+    });
+
     describe('URL Requests For bar = "foo"', function() {
-        var url = "http://localhost:3000/foo?bar=foo";
-        
+        var url = "https://team0x13.herokuapp.com/foo?bar=foo";
+
         it('returns status 200', function() {
             request(url, function(error, response, body){
                 assert.equal(response.statusCode,200);
             });
         });
-        
+
         it('return false', function() {
             request(url, function(error, response, body){
                 assert.equal(response.body, 'false');
             });
-        });  
-    }); 
-    
+        });
+    });
+
     describe('URL Requests For bar = "baz"', function() {
-        var url = "http://localhost:3000/foo?bar=foo";
-        
+        var url = "https://team0x13.herokuapp.com/foo?bar=foo";
+
         it('returns status 200', function() {
             request(url, function(error, response, body){
                 assert.equal(response.statusCode,200);
             });
         });
-        
+
         it('return true', function() {
             request(url, function(error, response, body){
                 assert.equal(response.body, 'true');
             });
-        });  
-    }); 
-    
-});     
+        });
+    });
 
-
+});
