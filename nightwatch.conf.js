4ca1f658fee44191402fc98d30a64bf1f042bd83
Eric Luo <eric_luo@Ymail.com>
2017-04-21 16:09:17 -0700

    added selenium driver for linux and mac

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -2,12 +2,14 @@ module.exports = (function (settings) {
  
   //Setting chromedriver path at runtime to run on different architectures
   if (process.platform === "darwin") {
-    settings.selenium.cli_args["webdriver.chrome.driver"] = "./node_modules/chromedriver/bin/chromedriver";
+    settings.selenium.cli_args["webdriver.chrome.driver"] = "./drivers/selenium/chrome/mac64/chromedriver.exe";
   }
   else if (process.platform === "win32" || process.platform === "win64") {
-    settings.selenium.cli_args["webdriver.chrome.driver"] = "./drivers/selenium/chrome/chromedriver.exe";
+    settings.selenium.cli_args["webdriver.chrome.driver"] = "./drivers/selenium/chrome/win32/chromedriver.exe";
   }
- 
+  else if (process.platform === "linux" ) {
+    settings.selenium.cli_args["webdriver.chrome.driver"] = "./drivers/selenium/chrome/linux32/chromedriver.exe";
+  }
   return settings;
  
 })(require('./nightwatch.json'));
