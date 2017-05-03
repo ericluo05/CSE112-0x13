59dd8053935bfab77143c59d7d1982be10029c69
Nicolas Lama-Solet <nicolas@lama-solet.com>
2017-05-02 21:21:25 -0700

    Added eslint support

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,19 +1,15 @@
-module.exports = function (settings) {
-
-  //Setting chromedriver path at runtime to run on different architectures
-  if (process.platform === "darwin") {
-    settings.selenium.cli_args["webdriver.chrome.driver"] = "./drivers/selenium/chrome/mac64/chromedriver";
-    settings.selenium.cli_args["webdriver.gecko.driver"] = "./drivers/selenium/firefox/mac64/geckodriver";
-    settings.selenium.cli_args["webdriver.safari.driver"] = "/usr/bin/safaridriver";
-  }
-  else if (process.platform === "win32" || process.platform === "win64") {
-    settings.selenium.cli_args["webdriver.chrome.driver"] = "./drivers/selenium/chrome/win32/chromedriver.exe";
-    settings.selenium.cli_args["webdriver.gecko.driver"] = "./drivers/selenium/firefox/win32/geckodriver.exe";
-  }
-  else if (process.platform === "linux" ) {
-    settings.selenium.cli_args["webdriver.chrome.driver"] = "./drivers/selenium/chrome/linux64/chromedriver";
-    settings.selenium.cli_args["webdriver.gecko.driver"] = "./drivers/selenium/firefox/linux64/geckodriver";
+module.exports = function(settings) {
+  // Setting chromedriver path at runtime to run on different architectures
+  if (process.platform === 'darwin') {
+    settings.selenium.cli_args['webdriver.chrome.driver'] = './drivers/selenium/chrome/mac64/chromedriver';
+    settings.selenium.cli_args['webdriver.gecko.driver'] = './drivers/selenium/firefox/mac64/geckodriver';
+    settings.selenium.cli_args['webdriver.safari.driver'] = '/usr/bin/safaridriver';
+  } else if (process.platform === 'win32' || process.platform === 'win64') {
+    settings.selenium.cli_args['webdriver.chrome.driver'] = './drivers/selenium/chrome/win32/chromedriver.exe';
+    settings.selenium.cli_args['webdriver.gecko.driver'] = './drivers/selenium/firefox/win32/geckodriver.exe';
+  } else if (process.platform === 'linux' ) {
+    settings.selenium.cli_args['webdriver.chrome.driver'] = './drivers/selenium/chrome/linux64/chromedriver';
+    settings.selenium.cli_args['webdriver.gecko.driver'] = './drivers/selenium/firefox/linux64/geckodriver';
   }
   return settings;
-
 }(require('./nightwatch.json'));
