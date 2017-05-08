a4d4a9978da79721741b28f7d5a55ec76c921303
Eric Luo <eric_luo@Ymail.com>
2017-05-08 03:36:55 -0700

    setup

--- doc/api_project.js
+++ doc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-08T09:04:28.136Z",
+    "time": "2017-05-08T10:19:41.047Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
