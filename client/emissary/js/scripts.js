618bb7e99897b071f382d86f39e0a6932368fd7f
kelseyma <kelsey.ma6@gmail.com>
2017-05-25 11:31:09 -0700

    fixed line length error

--- client/emissary/js/scripts.js
+++ client/emissary/js/scripts.js
@@ -10,7 +10,9 @@ jQuery(document).ready(function() {
 
   /* Form */
   $('.registration-form fieldset:first-child').fadeIn('slow');
-  $('.registration-form input[type="text"], .registration-form input[type="password"], .registration-form textarea').on('focus', function() {
+  $('.registration-form input[type="text"], ' +
+    '.registration-form input[type="password"], ' +
+    '.registration-form textarea').on('focus', function() {
     $(this).removeClass('input-error');
   });
 
@@ -19,7 +21,8 @@ jQuery(document).ready(function() {
     let parentFieldset = $(this).parents('fieldset');
     let nextStep = true;
 
-    parentFieldset.find('input[type="text"], input[type="password"], textarea').each(function() {
+    parentFieldset.find('input[type="text"], input[type="password"], ' +
+      'textarea').each(function() {
       if( $(this).val() == '' ) {
         $(this).addClass('input-error');
         nextStep = false;
