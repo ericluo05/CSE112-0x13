ec5e98b8c459b9bec5a38942c8998aa688c31273
Eric Luo <eric_luo@Ymail.com>
2017-05-29 12:00:07 -0700

    fixed admin acc not created due to async calls

--- server/app.js
+++ server/app.js
@@ -27,10 +27,8 @@ db.once('open', function callback() {
 });
 let Employee = require('./models/Employee');
 let Company = require('./models/Company');
+
 Company.findOne({name: 'Emissary'}, function(err, result) {
-    if (err) {
-        console.log(err);
-    }
     if(!result) {
         console.log('No Emissary Company found, creating one');
         let company = new Company();
@@ -43,49 +41,41 @@ Company.findOne({name: 'Emissary'}, function(err, result) {
                 console.log('error: unable to create/save Emissary company');
             }
         });
-    }
-    Employee.findOne({email: 'peter@emissary.com'}, function(err, result) {
-        if (err) {
-            console.log('no superadmin acc found, creating one. ' +
-                'acc: peter@emissary.com, pw:admin');
-        }
-        if(!result) {
-            let emissaryID = 0;
-            Company.findOne({name: 'Emissary'}, function(err, result) {
-                emissaryID = result._id;
-                let superadmin = new Employee();
-                superadmin.first_name = 'Peter';
-                superadmin.last_name = '???';
-                superadmin.email = 'peter@emissary.com',
-                    superadmin.phone_number = '5621234567',
-                    Company.findOne({name: 'Emissary'}, function(err, result) {
-                        console.log(result._id);
+        Employee.findOne({email: 'peter@emissary.com'}, function(err, result) {
+            if (err) {
+                console.log('no superadmin acc found, creating one. ' +
+                    'acc: peter@emissary.com, pw:admin');
+            }
+            if(!result) {
+                let emissaryID = 0;
+                Company.findOne({name: 'Emissary'}, function(err, result) {
+                    emissaryID = result._id;
+                    let superadmin = new Employee();
+                    superadmin.first_name = 'Peter';
+                    superadmin.last_name = '???';
+                    superadmin.email = 'peter@emissary.com',
+                        superadmin.phone_number = '5621234567',
+                        Company.findOne({name: 'Emissary'}, function(err, result) {
+                            console.log(result._id);
+                        });
+                    superadmin.company_id = emissaryID,
+                        superadmin.password = superadmin.generateHash('admin'),
+                        superadmin.role = 'a_admin';
+                    superadmin.save(function(err, e) {
+                        if (err) {
+                            console.log('error: unable to register superadmin account');
+                        }
                     });
-                superadmin.company_id = emissaryID,
-                    superadmin.password = superadmin.generateHash('admin'),
-                    superadmin.role = 'a_admin';
-                superadmin.save(function(err, e) {
-                    if (err) {
-                        console.log('error: unable to register superadmin account');
-                        console.log(err);
-                    }
                 });
-            });
-        }
-    });
-});
-
-
-Employee.findOne({email: 'peter@emissary.com'},  function(err, result) {
-   console.log(result);
+            }
+        });
+    }
 });
 
 
 // view engine setup
 app.set('views', path.join(__dirname + '/../build/views'));
 app.set('view engine', 'ejs');
-
-
 
 
 // these are not used in production environment
