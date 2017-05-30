af6df64f68d4691edffa49e3767fc310b71c341d
Toni Tran <toni.n.tran@gmail.com>
2017-05-30 13:56:14 -0700

    Added theme and visitorlist docs

--- server/routes/theme/index.js
+++ server/routes/theme/index.js
@@ -14,16 +14,48 @@ let controller = require('./theme.controller');
  */
 
 
-// post with default values
+/**
+ * @api {post} /:user_id/theme CreateNewDefaultUserTheme
+ * @apiName CreateNewDefaultUserTheme
+ * @apiGroup Theme
+ * @apiDescription Creates a new user theme with defaults.
+ * @apiParam {String} user_id The ID of the user that will get a new default theme.
+ * @apiSuccess {JSON} theme The theme of the user page, formatted as a JSON.
+ * @apiFailure {JSON} error The JSONified error code.
+ */
 router.post('/:user_id/theme', controller.create);
 
-// get the theme correspond to the user
+/**
+ * @api {get} /:user_id/theme GetUserTheme
+ * @apiName GetUserTheme
+ * @apiGroup Theme
+ * @apiDescription Gets the theme corresponding to some user.
+ * @apiParam {String} user_id The ID of the user whose theme we want to change.
+ * @apiSuccess {JSON} theme The theme of the user page, formatted as a JSON.
+ * @apiFailure {JSON} error The JSONified error code.
+ */
 router.get('/:user_id/theme', controller.get);
 
-// Edit, when the user save new settings
+/**
+ * @api {put} /:user_id/theme UpdateUserTheme
+ * @apiName UpdateUserTheme
+ * @apiGroup Theme
+ * @apiDescription Update the specific user's theme when they save their settings.
+ * @apiParam {String} user_id The ID of the user whose theme we want to change.
+ * @apiSuccess {JSON} theme The theme of the new user page theme, formatted as a JSON.
+ * @apiFailure {JSON} error The JSONified error code.
+ */
 router.put('/:user_id/theme', controller.update);
 
-// Delete, when a user unsuscribed from the service
+/**
+ * @api {put} /:user_id/theme DeleteUserTheme
+ * @apiName DeleteUserTheme
+ * @apiGroup Theme
+ * @apiDescription Deletes the user's theme, when they unsubsctibe from the service.
+ * @apiParam {String} user_id The ID of the user whose theme we want to change.
+ * @apiSuccess {JSON} msg "OK"
+ * @apiFailure {JSON} error The JSONified error code.
+ */
 router.delete('/:user_id/theme', controller.delete);
 
 module.exports = router;
