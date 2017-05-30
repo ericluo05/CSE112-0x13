f9c43eccf67e8ad178f34d14df259926db888e0d
Eric Luo <eric_luo@Ymail.com>
2017-05-30 14:02:05 -0700

    timeout before creating super admin

--- server/createsuperadmin.js
+++ server/createsuperadmin.js
@@ -1,53 +1,48 @@
-
+'use strict';
 let Employee = require('./models/Employee');
 let Company = require('./models/Company');
 
-Company.findOne({name: 'Emissary'}, function(err, result, done) {
-    if(!result) {
-        console.log('No Emissary Company found, creating one');
-        let company = new Company();
-        company.email = 'support@emissary.com';
-        company.name = 'Emissary';
-        company.phone_number = '9119119110';
-        company.paid_time=new Date();
-        company.save(function(err, c, done) {
-            if(err) {
-                console.log('error: unable to create/save Emissary company');
-            }
-        });
-        Employee.findOne({email: 'peter@emissary.com'}, function(err, result) {
-            if (err) {
-                console.log('error try to finding peter@emissary.com in database', +err);
-            }
-            if(!result) {
-                console.log('no superadmin acc found, creating one. ' +
-                    'acc: peter@emissary.com, pw:admin');
-                createSuperAdminAcc();
-            }
-        });
-    }
-});
+createEmissaryCompany();
+setTimeout(createSuperAdminAcc, 3000);
+/**
+ * @private
+ * @function createEmissaryCompany
+ * @description  create emissary company
+ */
+function createEmissaryCompany() {
+    Company.findOne({name: 'Emissary'}, function(err, result) {
+        if(!result) {
+            console.log('No Emissary Company found, creating one');
+            let company = new Company();
+            company.email = 'support@emissary.com';
+            company.name = 'Emissary';
+            company.phone_number = '9119119110';
+            company.paid_time=new Date();
+            company.save(function(err, c) {
+                if(err) {
+                    console.log('error: unable to create/save Emissary company');
+                }
+                console.log(c);
+            });
+        }
+    });
+}
+
+
 /**
  * @private
  * @function createSuperAdminAcc
  * @description  create dummy super admin account peter@emissary.com
- * @param {string} company_id - id of the company
- * @param {string} visitor_name - name of visitor
  */
 function createSuperAdminAcc() {
-    let emissaryID = 0;
     Company.findOne({name: 'Emissary'}, function(err, result) {
-        emissaryID = result._id;
         let superadmin = new Employee();
         superadmin.first_name = 'Peter';
         superadmin.last_name = '???';
-        superadmin.email = 'peter@emissary.com',
-        superadmin.phone_number = '5621234567',
-        Company.findOne({name: 'Emissary'}, function(err, result) {
-           console.log('Emissary company id:'+ result._id);
-        });
-        superadmin.company_id = emissaryID,
-        superadmin.password = superadmin.generateHash('admin'),
+        superadmin.email = 'peter@emissary.com';
+        superadmin.phone_number = '5621234567';
+        superadmin.company_id = result._id;
+        superadmin.password = superadmin.generateHash('admin');
         superadmin.role = 'a_admin';
         superadmin.save(function(err, e) {
             if (err) {
