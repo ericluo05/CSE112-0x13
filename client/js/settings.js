de95331db5618f7003b7173e5da910bcdf235a2e
Eric Luo <eric_luo@Ymail.com>
2017-05-31 15:19:13 -0700

    [ci skip]moving files from emissary folder to general folder
    will start migrating on Weekend for easier maintenance

---
+++ client/js/settings.js
@@ -0,0 +1,41 @@
+window.onload = function() {
+  /**
+    * Grab user data from form
+    * @param {Object} sParam
+    * @return {Object}
+    */
+  function getURLParameter(sParam) {
+    let sPageURL = window.location.search.substring(1);
+    let sURLVariables = sPageURL.split('&');
+    for (let i = 0; i < sURLVariables.length; i++) {
+        let sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return decodeURIComponent(sParameterName[1]);
+        }
+    }
+}
+let id = getURLParameter('code');
+// let name= getURLParameter('status');
+
+if(id) {
+	let clientSecret = 'a56caa836b9f386c400a69cda6f5c0b4';
+	let clientID = '11293703667.22923580850';
+	// let something = {};
+	$.post('https://slack.com/api/oauth.access',
+    {
+        'client_id': clientID,
+        'client_secret': clientSecret,
+        'code': id,
+    },
+    function(data, status) {
+        // alert("Data: " + data + "\nStatus: " + status);
+        console.log(data);
+        let webhook = data['incoming_webhook'];
+        let channel = webhook['channel'];
+        console.log('webhook:' + channel);
+        console.log('token' + data['access_token']);
+        localStorage.setItem('slackToken', data['access_token']);
+        localStorage.setItem('slackChannel', channel);
+    });
+}
+};
