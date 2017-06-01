db385c9261444d9eb2164b549b648fd95753f0a8
Eric Luo <eric_luo@Ymail.com>
2017-05-31 22:31:41 -0700

    this should fix the issue where admin acc is not created if Emissary company exists

--- server/createsuperadmin.js
+++ server/createsuperadmin.js
@@ -11,7 +11,11 @@ setTimeout(createSuperAdminAcc, 3000);
  */
 function createEmissaryCompany() {
     Company.findOne({name: 'Emissary'}, function(err, result) {
-        if(err || !result) {
+        if(err ) {
+           return console.log('Error trying to create Emissary' +
+               ' Company: ' + err.code);
+        }
+        if(!result) {
             console.log('No company named "Emissary" found, creating one');
             let company = new Company();
             company.email = 'support@emissary.com';
@@ -24,6 +28,8 @@ function createEmissaryCompany() {
                 if(c)
                     EmissaryCompanyID = c._id;
             });
+        } else{
+            EmissaryCompanyID = result._id;
         }
     });
 }
@@ -36,19 +42,26 @@ function createEmissaryCompany() {
  */
 function createSuperAdminAcc() {
      Employee.findOne({email: 'peter@emissary.com'}, function(err, result) {
-        if(err || !result) {
+        if(err) {
+            return console.log('Error trying to find super-admin' +
+                ' account: ' + err.message);
+        }
+        if(!result) {
             console.log('No SuperAdminAcc "peter@emissary.com" found, creating one');
             let superadmin = new Employee();
             superadmin.first_name = 'Peter';
             superadmin.last_name = 'Venkman';
             superadmin.email = 'peter@emissary.com';
             superadmin.phone_number = '5621234567';
+            if(EmissaryCompanyID ===0) {
+                console.log('Can\'t find EmissaryCompanyID, this shouldn\'t show up');
+            }
             superadmin.company_id = EmissaryCompanyID;
             superadmin.password = superadmin.generateHash('admin');
             superadmin.role = 'a_admin';
             superadmin.save(function(err, e) {
                 if (err) {
-                    console.log('error: unable to register superadmin account');
+                    console.log('Error unable to register superadmin account: '+err.code);
                 }
             });
         }
