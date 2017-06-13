4af68296457328d6dd6eee7eea5ff13decd405ef
kelseyma <kelsey.ma6@gmail.com>
2017-06-12 22:20:29 -0700

    added change password functionality

--- client/js/admin-settings.js
+++ client/js/admin-settings.js
@@ -6,6 +6,10 @@ $(document).ready(function() {
   $('#user-name').text(curUser.first_name);
   showInfo();
 
+  jQuery(function($) {
+    $('#modal-phone').mask('(999) 999-9999');
+  });
+
   $('#modal-first').val(curUser.first_name);
   $('#modal-last').val(curUser.last_name);
   $('#modal-email').val(curUser.email);
@@ -14,23 +18,47 @@ $(document).ready(function() {
   $('.save-btn').click(updateInfo);
 
   /**
-    * Uses POST request to update user account information
+    * Uses Ajax request to update user account information
     **/
   function updateInfo() {
     let newVals = grabFormValues();
-    $.ajax({
-      dataType: 'json',
-      type: 'PUT',
-      data: newVals,
-      async: false,
-      url: 'api/employees/' + curUser._id,
-      success: function(response) {
-        console.log(response);
-        localStorage.setItem('currentUser', JSON.stringify(response));
-        curUser = JSON.parse(localStorage.getItem('currentUser'));
-        showInfo();
-      },
-    });
+    if(newVals.first_name != "" && newVals.last_name != "" &&
+      newVals.email != "" && newVals.phone_number != "") {
+      let editInfo = {};
+      editInfo.first_name= newVals.first_name;
+      editInfo.last_name = newVals.last_name;
+      editInfo.email = newVals.email;
+      editInfo.phone_number = newVals.phone_number;
+      $.ajax({
+        dataType: 'json',
+        type: 'PUT',
+        data: editInfo,
+        url: 'api/employees/' + curUser._id,
+        success: function(response) {
+          console.log(response);
+          localStorage.setItem('currentUser', JSON.stringify(response));
+          curUser = JSON.parse(localStorage.getItem('currentUser'));
+          showInfo();
+        },
+      });
+    }
+
+    if(newVals.curr_pw != ""&&newVals.new_pw != ""&&newVals.repeat_pw != "") {
+      let editInfo = {};
+      editInfo.currentpwd = newVals.curr_pw;
+      editInfo.newpwd = newVals.new_pw;
+      $.ajax({
+        dataType: 'json',
+        type: 'POST',
+        data: jQuery.param(editInfo),
+        url: 'api/employees/pwdchange/' + curUser._id,
+        success: function(response) {
+          console.log(response);
+          localStorage.setItem('currentUser', JSON.stringify(response));
+          curUser = JSON.parse(localStorage.getItem('currentUser'));
+        }
+      });
+    }
   }
 
   /**
@@ -50,10 +78,13 @@ $(document).ready(function() {
     **/
   function grabFormValues() {
     let newInfo = {};
-    newInfo.first_name= $('#modal-first').val();
+    newInfo.first_name = $('#modal-first').val();
     newInfo.last_name = $('#modal-last').val();
     newInfo.email = $('#modal-email').val();
     newInfo.phone_number = $('#modal-phone').val();
+    newInfo.curr_pw = $('#modal-curr-pw').val();
+    newInfo.new_pw = $('#modal-new-pw').val();
+    newInfo.repeat_pw = $('#modal-repeat-new-pw').val();
 
     return newInfo;
   }
