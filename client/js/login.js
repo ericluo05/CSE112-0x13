676348d3fb86f6ecfe2be378f9bf3e5c1cdc3bc6
Eric Luo <eric_luo@Ymail.com>
2017-06-14 02:06:02 -0700

    fixed login for firefox

--- client/js/login.js
+++ client/js/login.js
@@ -1,89 +1,65 @@
-// with Button named loginButton
-$(function() {
-   $('#loginButton').click(function() {
-       let userData = grabUserData();
-       // alert(userData);
-       event.preventDefault();
-       ajaxPostUser('/api/employees/login', userData);
-   });
-});
-
-
 // with Button named signin-bt
 $(function() {
-   $('#logoutButton').click(function() {
-       localStorage.removeItem('userState');
-       localStorage.removeItem('currentUser');
-       localStorage.removeItem('currentCompany');
-   });
+    $('#logoutButton').click(function() {
+        localStorage.removeItem('userState');
+        localStorage.removeItem('currentUser');
+        localStorage.removeItem('currentCompany');
+    });
 });
 
-/**
-  * Ajax function to create a POST request to server
-  * @param {Object} url
-  * @param {Object} data
-  */
-function ajaxPostUser(url, data) {
-   $.ajax({
-       type: 'POST',
-       url: url,
-       data: data,
-       dataType: 'json',
-       success: function(response) {
-           console.log(response);
-           if(response.role == 'a_admin') {
-             localStorage.setItem('userState', 2);
-             location.href = '/admin-panel.html';
-           } else{
-             localStorage.setItem('userState', 1);
-             location.href = '/visitors.html';
-           }
-           localStorage.setItem('currentUser', JSON.stringify(response));
-           ajaxGetCompanyInfo('/api/companies/' + response.company_id);
-       },
-       error: function() {
-           window.onerror=handleError();
-           event.preventDefault();
-           // location.href = '/login.html';
+/** Ajax function to create a POST request to server, used by neon-login */
+function ajaxPostUser() {
+    $('#loginButton').prop('disabled', true);
+    $('#username').prop('disabled', true);
+    $('#password').prop('disabled', true);
+    $.ajax({
+        type: 'POST',
+        url: '/api/employees/login',
+        data: grabUserData(),
+        dataType: 'json',
+        success: function(response) {
+            if (response.role == 'a_admin') {
+                localStorage.setItem('userState', 2);
+                location.href = '/admin-panel.html';
+            } else {
+                localStorage.setItem('userState', 1);
+                location.href = '/visitors.html';
+            }
+            localStorage.setItem('currentUser', JSON.stringify(response));
+            ajaxGetCompanyInfo('/api/companies/' + response.company_id);
         },
-   });
+        error: function() {
+            $('#errorlog').html('Invalid Username or Password');
+            $('#loginButton').prop('disabled', false);
+            $('#username').prop('disabled', false);
+            $('#password').prop('disabled', false);
+        },
+    });
 }
 
 /**
-  * Ajax function to create a GET request to server
-  * @param {Object} url
-  */
+ * Ajax function to create a GET request to server
+ * @param {Object} url
+ */
 function ajaxGetCompanyInfo(url) {
-   $.ajax({
-       type: 'GET',
-       url: url,
-       data: $('#response').serialize(),
-       async: false,
-       dataType: 'json',
-       success: function(response) {
-           console.log(response);
-           // alert(response.name);
-           localStorage.setItem('currentCompany', JSON.stringify(response));
-       },
-   });
+    $.ajax({
+        type: 'GET',
+        url: url,
+        data: $('#response').serialize(),
+        dataType: 'json',
+        success: function(response) {
+            localStorage.setItem('currentCompany', JSON.stringify(response));
+        },
+    });
 }
 
 /**
-  * Grab user data from form
-  * @return {Object} user
-  */
+ * Grab user data from form
+ * @return {Object} user
+ */
 function grabUserData() {
-   let user = {};
-   user.email = $('#username').val();
-   user.password = $('#password').val();
-   return user;
-}
-
-/**
-  * Grab user data from form
-  * @return {Boolean}
-  */
-function handleError() {
-   errorlog.innerHTML='Not Valid Username and Password, please type valid one.';
-   return true;
+    let user = {};
+    user.email = $('#username').val();
+    user.password = $('#password').val();
+    return user;
 }
