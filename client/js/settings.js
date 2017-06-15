4f572cd43a40e5fa64b24d6abdcb8ffa7ad2d439
Eric Luo <eric_luo@Ymail.com>
2017-06-15 16:23:49 -0700

    reformatted settings.js code

--- client/js/settings.js
+++ client/js/settings.js
@@ -1,124 +1,125 @@
-window.onload = function() {
- /**
-   * Grab user data from form
-   * @param {Object} sParam
-   * @return {Object}
-   */
- function getURLParameter(sParam) {
-   let sPageURL = window.location.search.substring(1);
-   let sURLVariables = sPageURL.split('&');
-   for (let i = 0; i < sURLVariables.length; i++) {
-       let sParameterName = sURLVariables[i].split('=');
-       if (sParameterName[0] == sParam) {
-           return decodeURIComponent(sParameterName[1]);
-       }
-   }
-}
-let id = getURLParameter('code');
+window.onload = function () {
+    /**
+     * Grab user data from form
+     * @param {Object} sParam
+     * @return {Object}
+     */
+    function getURLParameter(sParam) {
+        let sPageURL = window.location.search.substring(1);
+        let sURLVariables = sPageURL.split('&');
+        for (let i = 0; i < sURLVariables.length; i++) {
+            let sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] == sParam) {
+                return decodeURIComponent(sParameterName[1]);
+            }
+        }
+    }
+
+    let id = getURLParameter('code');
 // let name= getURLParameter('status');
 
-if(id) {
-	let clientSecret = '9849d0f5a2a2a107664bf0f83ec63bad';
-	let clientID = '168056792887.192343525634';
-	// let something = {};
-	$.post('https://slack.com/api/oauth.access',
-   {
-       'client_id': clientID,
-       'client_secret': clientSecret,
-       'code': id,
-   },
-   function(data, status) {
-       // alert("Data: " + data + "\nStatus: " + status);
-       console.log(data);
-       let webhook = data['incoming_webhook'];
-       let channel = webhook['channel'];
-       console.log('webhook:' + channel);
-       console.log('token' + data['access_token']);
-       localStorage.setItem('slackToken', data['access_token']);
-       localStorage.setItem('slackChannel', channel);
-   });
-}
+    if (id) {
+        let clientSecret = '9849d0f5a2a2a107664bf0f83ec63bad';
+        let clientID = '168056792887.192343525634';
+        // let something = {};
+        $.post('https://slack.com/api/oauth.access',
+            {
+                'client_id': clientID,
+                'client_secret': clientSecret,
+                'code': id,
+            },
+            function (data, status) {
+                // alert("Data: " + data + "\nStatus: " + status);
+                console.log(data);
+                let webhook = data['incoming_webhook'];
+                let channel = webhook['channel'];
+                console.log('webhook:' + channel);
+                console.log('token' + data['access_token']);
+                localStorage.setItem('slackToken', data['access_token']);
+                localStorage.setItem('slackChannel', channel);
+            });
+    }
 };
 
 
-$(document).ready(function() {
-  let myCompanyId = '';
-  let stripeToken = '';
-  let curUser = JSON.parse(localStorage.getItem('currentUser'));
-  let companyData = JSON.parse(localStorage.getItem('currentCompany'));
-  myCompanyId = companyData._id;
-  $('#user-name').text(curUser.first_name + ' ' + curUser.last_name);
-
-  jQuery(function($) {
-    $('#modal-phone').mask('(999) 999-9999');
-  });
-
-  showInfo();
-  $('#modal-first').val(curUser.first_name);
-  $('#modal-last').val(curUser.last_name);
-  $('#modal-email').val(curUser.email);
-  $('#modal-phone').val(curUser.phone_number);
-
-  $('.save-btn').click(updateInfo);
-
-  /**
-    * Uses POST request to update user account information
-    **/
-  function updateInfo() {
-    let newVals = grabFormValues();
-    $.ajax({
-      dataType: 'json',
-      type: 'PUT',
-      data: newVals,
-      async: false,
-      url: 'api/employees/' + curUser._id,
-      success: function(response) {
-        console.log(response);
-        localStorage.setItem('currentUser', JSON.stringify(response));
-        curUser = JSON.parse(localStorage.getItem('currentUser'));
-        showInfo();
-      },
-    });
-  }
-
-  /**
-    * Use current user saved in local storage to show user information
-    **/
-  function showInfo() {
+$(document).ready(function () {
+    let myCompanyId = '';
+    let stripeToken = '';
+    let curUser = JSON.parse(localStorage.getItem('currentUser'));
+    let companyData = JSON.parse(localStorage.getItem('currentCompany'));
+    myCompanyId = companyData._id;
     $('#user-name').text(curUser.first_name + ' ' + curUser.last_name);
-    $('#first-name').text(curUser.first_name);
-    $('#last-name').text(curUser.last_name);
-    $('#email').text(curUser.email);
-    $('#phone').text(curUser.phone_number);
-  }
 
-  /**
-    * Grabs elements from the form and puts it into an object
-    * @return {object}
-    **/
-  function grabFormValues() {
-    let newInfo = {};
-    newInfo.first_name= $('#modal-first').val();
-    newInfo.last_name = $('#modal-last').val();
-    newInfo.email = $('#modal-email').val();
-    newInfo.phone_number = $('#modal-phone').val();
+    jQuery(function ($) {
+        $('#modal-phone').mask('(999) 999-9999');
+    });
 
-    return newInfo;
-  }
+    showInfo();
+    $('#modal-first').val(curUser.first_name);
+    $('#modal-last').val(curUser.last_name);
+    $('#modal-email').val(curUser.email);
+    $('#modal-phone').val(curUser.phone_number);
 
-  $('#logoutButton').on('click', function() {
-    localStorage.setItem('userState', 0);
-  });
+    $('.save-btn').click(updateInfo);
+
+    /**
+     * Uses POST request to update user account information
+     **/
+    function updateInfo() {
+        let newVals = grabFormValues();
+        $.ajax({
+            dataType: 'json',
+            type: 'PUT',
+            data: newVals,
+            async: false,
+            url: 'api/employees/' + curUser._id,
+            success: function (response) {
+                console.log(response);
+                localStorage.setItem('currentUser', JSON.stringify(response));
+                curUser = JSON.parse(localStorage.getItem('currentUser'));
+                showInfo();
+            },
+        });
+    }
+
+    /**
+     * Use current user saved in local storage to show user information
+     **/
+    function showInfo() {
+        $('#user-name').text(curUser.first_name + ' ' + curUser.last_name);
+        $('#first-name').text(curUser.first_name);
+        $('#last-name').text(curUser.last_name);
+        $('#email').text(curUser.email);
+        $('#phone').text(curUser.phone_number);
+    }
+
+    /**
+     * Grabs elements from the form and puts it into an object
+     * @return {object}
+     **/
+    function grabFormValues() {
+        let newInfo = {};
+        newInfo.first_name = $('#modal-first').val();
+        newInfo.last_name = $('#modal-last').val();
+        newInfo.email = $('#modal-email').val();
+        newInfo.phone_number = $('#modal-phone').val();
+
+        return newInfo;
+    }
+
+    $('#logoutButton').on('click', function () {
+        localStorage.setItem('userState', 0);
+    });
 
     let handler = StripeCheckout.configure({
         key: 'pk_test_b6iDPAQ2gLMWOr6zCHKtwXEq',
         image: '/images/appt-o-matic.png',
         locale: 'auto',
-        token: function(token) {
+        token: function (token) {
             makePayment(token);
         },
     });
-    document.getElementById('subscribe-btn').addEventListener('click', function(e) {
+    document.getElementById('subscribe-btn').addEventListener('click', function (e) {
         // Open Checkout with further options:
         handler.open({
             name: 'Appt-o-matic',
@@ -131,7 +132,7 @@ $(document).ready(function() {
     });
 
 // Close Checkout on page navigation:
-    window.addEventListener('popstate', function() {
+    window.addEventListener('popstate', function () {
         handler.close();
     });
 
@@ -150,10 +151,10 @@ $(document).ready(function() {
             async: false,
             url: '/payment/subscription/' + myCompanyId,
             data: queryInfo,
-            success: function(response) {
+            success: function (response) {
                 $('#result-msg').html(response.message);
             },
-            error: function(response) {
+            error: function (response) {
                 $('#result-msg').html(JSON.parse(response.responseText).error);
             },
         });
