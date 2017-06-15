3db9d5d2a2b1d42da48b7c5079aea3a8245746f2
Eric Luo <eric_luo@Ymail.com>
2017-06-15 06:13:51 -0700

    [ci skip] disable collapseWhitespace for htmlmin

--- gulp/build.js
+++ gulp/build.js
@@ -52,7 +52,7 @@ gulp.task('copy:html', function() {
     return gulp.src(paths.html_src)
         .pipe(newer(paths.html_dest))
         .pipe((process.env.NODE_ENV === 'production')?
-            htmlmin({collapseWhitespace: true}):util.noop())
+            htmlmin({collapseWhitespace: false}):util.noop())
         .pipe(gulp.dest(paths.html_dest));
 });
 
